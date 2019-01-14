0601b2e7c5f82198c82ce547ebcc69d8b97ab22c
muokicaleb <muokicaleb@gmail.com>
2019-01-14 20:58:23 +0300

    lesson 2

--- Flask_webDevelopment_OREILLY/social_blog/flaskblog.py
+++ Flask_webDevelopment_OREILLY/social_blog/flaskblog.py
@@ -3,14 +3,30 @@ from flask import render_template
 
 app = Flask(__name__)
 
+posts = [
+        {
+            'author':'John Smith',
+            'title':'first post',
+            'content': '1st post content',
+            'date_posted': 'Jan 14 1996'
+            },
+        { 
+            'author':'Jane Doe',            
+            'title':'2nd post',
+            'content': '2nd post content', 
+            'date_posted': 'Jan 14 1996'
+            }
+        ]
+
+
 @app.route("/")
 @app.route("/index")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == "__main__":
