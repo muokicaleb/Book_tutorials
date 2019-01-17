a67d1151600cd9c4e768d1611cc288b65b97d0b9
muokicaleb <muokicaleb@gmail.com>
2019-01-17 11:56:46 +0300

    lesson3

--- Flask_webDevelopment_OREILLY/social_blog/flaskblog.py
+++ Flask_webDevelopment_OREILLY/social_blog/flaskblog.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask 
+from forms import RegistrationForm, LoginForm
 from flask import render_template
 from flask import url_for
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '315742e55f9228cea7c3d52418bda5e3'
 
 posts = [
         {
@@ -25,11 +28,21 @@ posts = [
 def hello():
     return render_template('home.html', posts=posts)
 
+
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',title='Register', form=form)
+
+@app.route("/login")
+def login:
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
 if __name__ == "__main__":
     app.run(debug=True)
-
