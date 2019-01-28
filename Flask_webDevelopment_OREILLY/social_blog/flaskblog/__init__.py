7ed62adbbacd42297f51a6d615666dbbd4818344
muokicaleb <muokicaleb@gmail.com>
2019-01-28 21:25:27 +0300

    lesson5

--- Flask_webDevelopment_OREILLY/social_blog/flaskblog/__init__.py
+++ Flask_webDevelopment_OREILLY/social_blog/flaskblog/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -8,5 +8,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '315742e55f9228cea7c3d52418bda5e3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
--- Flask_webDevelopment_OREILLY/social_blog/flaskblog/routes.py
+++ Flask_webDevelopment_OREILLY/social_blog/flaskblog/routes.py
@@ -3,6 +3,8 @@ from flask import url_for
 from flask import flash
 from flask import redirect
 from flaskblog import app
+from flaskblog import db
+from flaskblog import bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
 
@@ -37,8 +39,13 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+
+        db.session.commit()
+        flash(f'Account has been created!', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
