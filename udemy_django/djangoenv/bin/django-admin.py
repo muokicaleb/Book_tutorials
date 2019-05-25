723b192c4ea6bfbfc6a3f2e34797d08de1cfefac
muokicaleb <muokicaleb@gmail.com>
2019-05-25 11:15:13 +0100

    django env

---
+++ udemy_django/djangoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/muoki/data/documents/code_training/portfolio/book_tutorials/udemy_django/djangoenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
